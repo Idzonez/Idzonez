92630ef2200b8a97c589f013acdf92ea8d1dd197
Idzonez <idzonez@yahoo.com>
2025-01-06 12:49:44 -0600

    Update server.js

--- IDZonez/wwwroot/server.js
+++ IDZonez/wwwroot/server.js
@@ -112,7 +112,7 @@ app.post('/api/auth/validate-admin', (req, res) => {
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).json({ 
-        message: 'Something blud!',
+        message: 'Something went wrong!',
         error: process.env.NODE_ENV === 'development' ? err.message : undefined
     });
 });
